3b9b36755618a91e4d56f178a65fcfd87834fec1
ronshuvy <rshuvy@gmail.com>
2021-03-06 16:45:57 +0200

    1c completed

--- client/src/components/Ticket.tsx
+++ client/src/components/Ticket.tsx
@@ -17,21 +17,13 @@ class Ticket extends React.Component<TicketProps, TicketState> {
         this.handleTicketHide = this.handleTicketHide.bind(this);
     }
 
-    contentText: React.RefObject<unknown> = React.createRef();
-
     handleTicketHide() {
         this.props.onHideTicket(this.props.ticket.id);
-    }
-
-    getLinesCount() {
-        const node = this.contentText.current;
-        console.log(node);
     }
 
     render() {
         const { ticket } = this.props;
         const labels = ticket.labels;
-        this.getLinesCount();
 
         return (
             <div className="ticket">
@@ -39,9 +31,7 @@ class Ticket extends React.Component<TicketProps, TicketState> {
                     Hide
                 </button>
                 <h5 className="title">{ticket.title}</h5>
-                <p className="content" ref={this.contentText}>
-                    {ticket.content}
-                </p>
+                <p className="content">{ticket.content}</p>
                 <footer>
                     <div className="meta-data">
                         By {ticket.userEmail} | {new Date(ticket.creationTime).toLocaleString()}
